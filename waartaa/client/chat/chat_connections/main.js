ed664b98356993922e40c1458f9fb368dc8bbff6
Ratnadeep Debnath <rtnpro@gmail.com>
2014-01-24 03:16:23 +0530

    Show 'add server' modal when no server has been added by user.

--- waartaa/client/chat/chat_connections/main.js
+++ waartaa/client/chat/chat_connections/main.js
@@ -1,3 +1,10 @@
 Template.chat_connections.servers = function () {
   return UserServers.find();
 }
+
+Template.chat_connections.created = function () {
+    Meteor.setTimeout(function () {
+        if (UserServers.find().count() == 0)
+            $('#server-add-btn').click();
+    }, 1000);
+}
