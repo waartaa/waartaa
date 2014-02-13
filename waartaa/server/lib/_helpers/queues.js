05d5d45f199534799aa5526c4d8b0d72912087b6
Ratnadeep Debnath <rtnpro@gmail.com>
2014-02-14 04:01:23 +0530

    Run enqueue task operation inside Fiber.

--- waartaa/server/lib/_helpers/queues.js
+++ waartaa/server/lib/_helpers/queues.js
@@ -1,13 +1,21 @@
 enqueueTask = function (queue, func, timeout) {
-    var timeout = timeout || CONFIG.DEFAULT_ASYNC_TASK_TIMEOUT || 5000;
-    queue.add(function (done) {
-        var timeoutId = Meteor.setTimeout(function () {
-            done();
-        }, timeout);
-        Fiber(function () {
-            func();
-            done();
-            Meteor.clearTimeout(timeoutId);
-        }).run();
-    });
+    Fiber(function () {
+        var timeout = timeout || CONFIG.DEFAULT_ASYNC_TASK_TIMEOUT || 5000;
+        queue.add(function (done) {
+            try {
+                var timeoutId = Meteor.setTimeout(function () {
+                    done();
+                }, timeout);
+                Fiber(function () {
+                    func();
+                    done();
+                    Meteor.clearTimeout(timeoutId);
+                }).run();
+            } catch (err) {
+                console.log(err, err.stack);
+                done();
+                Meteor.clearTimeout(timeoutId);
+            }
+        });
+    }).run();
 };
