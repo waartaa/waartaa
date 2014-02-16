e9ecdac11b7965e00f5eecbcb138afcceb366b97
Ratnadeep Debnath <rtnpro@gmail.com>
2014-02-16 13:25:15 +0530

    Set server datetime as channel log's created/last_updated values.

--- waartaa/server/perms.js
+++ waartaa/server/perms.js
@@ -1,10 +1,9 @@
 UserChannelLogs.allow({
   insert: function (userId, log) {
-    console.log(log);
     log.status = 'sent';
-    console.log('+++++USERID+++++', userId);
+    log.created = new Date();
+    log.last_updated = new Date();
     var user = Meteor.users.findOne({_id: userId});
-    console.log(user);
     return _send_channel_message(
         user, log.channel_id, log.message, {log: false});
   }
