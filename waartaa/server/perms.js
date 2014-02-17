fbf361e84d81bb0111bc3029c6808cf5c0506bbc
Ratnadeep Debnath <rtnpro@gmail.com>
2014-02-17 15:00:54 +0530

    Fixed logging /me messages.

--- waartaa/server/perms.js
+++ waartaa/server/perms.js
@@ -6,8 +6,12 @@ UserChannelLogs.allow({
         _send_channel_message(
         user, log.channel_id, log.message, {log: false});
     }, 5);
-    if (log.message[0] == '/')
+    if (log.message[0] == '/' && log.message.substr(0, 3) != '/me')
         return false;
+    if (log.message.substr(0, 3) == '/me') {
+        log.message = log.message.replace('/me', log.from);
+        log.from = null;
+    }
     return true;
   }
 });
