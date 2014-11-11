02290d49fc771a31f0f6154e3f219ff5f28d76e9
Ratnadeep Debnath <rtnpro@gmail.com>
2014-11-11 07:56:27 +0530

    Replaced glyphicons with fontawesome icons.

--- app/client/chat/chat_connections/_helpers.js
+++ app/client/chat/chat_connections/_helpers.js
@@ -2,13 +2,13 @@ UI.registerHelper('showStatusIcon', function (status) {
   var iconClass = "";
   var statusIconHtml = '';
   if (status == 'connected')
-    iconClass = 'glyphicon-ok-circle';
+    iconClass = 'fa-check-circle-o';
   else if (status == 'disconnected' || status == 'user_disconnected')
-    iconClass = 'glyphicon-ban-circle';
+    iconClass = 'fa-ban';
   else if (status == 'connecting' || status == 'disconnecting')
-    iconClass = 'spin glyphicon-refresh';
+    iconClass = 'spin fa-refresh';
   if (iconClass) {
-    statusIconHtml = '<icon class="tipsy-enable glyphicon ' + iconClass + '" tooltip="'
+    statusIconHtml = '<icon class="tipsy-enable fa ' + iconClass + '" tooltip="'
       + status + '"></icon>';
   }
   return new Spacebars.SafeString(statusIconHtml);
