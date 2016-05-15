f1d12716e65b29248c5898f6680d7e8c6faa2f4f
Ratnadeep Debnath <rtnpro@gmail.com>
2016-05-15 17:00:41 +0530

    Wired up pyramid project with client side react code.

--- waartaa/client/webpack.config.js
+++ waartaa/client/webpack.config.js
@@ -7,7 +7,7 @@ const NpmInstallPlugin = require('npm-install-webpack-plugin');
 const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, 'app'),
-  build: path.join(__dirname, 'build')
+  build: path.join(__dirname, '../static/js')
 };
 
 process.env.BABEL_ENV = TARGET;
@@ -21,7 +21,7 @@ const common = {
   },
   output: {
     path: PATHS.build,
-    filename: 'bundle.js'
+    filename: 'waartaa.js'
   },
   module: {
     loaders: [
