034598544e5cd7030833986217431d1d88d32c6e
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2016-05-15 18:27:55 +0530

    Add missing files and fix setup.py

--- setup.py
+++ setup.py
@@ -3,40 +3,43 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.txt')) as f:
+with open(os.path.join(here, 'README.md')) as f:
     README = f.read()
-with open(os.path.join(here, 'CHANGES.txt')) as f:
+
+with open(os.path.join(here, 'CHANGELOG.md')) as f:
     CHANGES = f.read()
 
-requires = [
-    'pyramid',
-    'pyramid_chameleon',
-    'pyramid_debugtoolbar',
-    'waitress',
-    ]
 
-setup(name='waartaa',
-      version='0.0',
-      description='waartaa',
-      long_description=README + '\n\n' + CHANGES,
-      classifiers=[
+def parse_requirements(requirements_file):
+    with open(requirements_file, 'r') as f:
+        return [line for line in f if line.strip() and not line.startswith('#')]
+
+REQUIREMENTS_PATH = 'requirements.txt'
+
+setup(
+    nae='waartaa',
+    version='0.1.1',
+    description='waartaa',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
         "Programming Language :: Python",
         "Framework :: Pyramid",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        ],
-      author='',
-      author_email='',
-      url='',
-      keywords='web pyramid pylons',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=requires,
-      tests_require=requires,
-      test_suite="waartaa",
-      entry_points="""\
-      [paste.app_factory]
-      main = waartaa:main
-      """,
-      )
+    ],
+    author='',
+    author_email='',
+    url='',
+    keywords='web pyramid pylons',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=parse_requirements(REQUIREMENTS_PATH),
+    tests_require=parse_requirements(REQUIREMENTS_PATH),
+    test_suite="waartaa",
+    entry_points={
+        'paste.app_factory': [
+            "main = waartaa:main"
+        ]
+    }
+)
