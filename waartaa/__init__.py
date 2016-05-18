c5a94ac1e5779e0168bdae2c0f66dabd593fca0b
Ratnadeep Debnath <rtnpro@gmail.com>
2016-05-18 13:07:47 +0530

    Configure backend pyramid server for rendering SPA.

--- waartaa/__init__.py
+++ waartaa/__init__.py
@@ -7,6 +7,7 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
+    config.add_route('api_v1', '/api/v1')
+    config.add_route('all', '/{all:.*}')
     config.scan()
     return config.make_wsgi_app()
