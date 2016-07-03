69d6943a80ac85dad3e5deef303fcb9a84334554
Ratnadeep Debnath <rtnpro@gmail.com>
2016-07-03 19:37:32 +0530

    Update crossbar config
    - remove hello.py crossbar component
    - move wsgi file for waartaa.

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from pyramid.paster import get_app, setup_logging
 
-ini_path = os.path.join(os.path.dirname(__file__), '../development.ini')
+ini_path = os.path.join(os.path.dirname(__file__), './development.ini')
 setup_logging(ini_path)
 application = get_app(ini_path, 'main')
