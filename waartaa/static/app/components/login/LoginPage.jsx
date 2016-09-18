25a2bfe81578883cf1d4a5890934ba490142388a
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2016-09-19 03:00:35 +0530

    Add the components as per mockup to the waartaa login page

--- waartaa/static/app/components/login/LoginPage.jsx
+++ waartaa/static/app/components/login/LoginPage.jsx
@@ -8,19 +8,37 @@ class LoginPage extends Component {
 
   render() {
     return (
-      <div class="{this.getTitle()}">
-        <div className="col-md-6">
-          <h3>Use an existing account</h3>
+      <div>
+        <div className="masthead">
+          <div className="container">
+            <div className="col-sm-3">
+              <h1>{this.getTitle()}</h1>
+            </div>
+          </div>
         </div>
-        <div className="col-md-6">
-          <h3>Create a Waartaa account</h3>
-          <form id="create-account-form">
-            <div className="form-group row"><input type="text" class="form-control" id="full-name" placeholder="Full Name" /></div>
-            <div className="form-group row"><input type="text" class="form-control" id="email" placeholder="Email" /></div>
-            <div className="form-group row"><input type="password" class="form-control" id="password" placeholder="Password" /></div>
-            <div className="form-group row"><input type="password" class="form-control" id="password-repeat" placeholder="Password repeat" /></div>
-            <div className="form-group row"><button type="submit" class="btn btn-primary">Create Account</button></div>
-          </form>
+        <div className="bodycontent m-h-75">
+          <div className="p-t-2">
+            <div className="container">
+              <div className="row">
+                <h2 className="text-xs-center p-t-2 p-b-3">Start using waartaa today.</h2>
+                <div className="col-md-6 m-x-auto text-xs-center">
+                  <h4 className="p-b-3">Use an existing account</h4>
+                </div>
+                <div className="col-md-6 m-x-auto text-xs-center left-border">
+                  <h4 className="p-b-3">Create a Waartaa account</h4>
+                  <div className="col-md-10 col-md-offset-1">
+                    <form id="create-account-form">
+                      <div className="form-group row"><input type="text" className="form-control" id="full-name" placeholder="Full Name" /></div>
+                      <div className="form-group row"><input type="text" className="form-control" id="email" placeholder="Email" /></div>
+                      <div className="form-group row"><input type="password" className="form-control" id="password" placeholder="Password" /></div>
+                      <div className="form-group row"><input type="password" className="form-control" id="password-repeat" placeholder="Password repeat" /></div>
+                      <div className="form-group row"><button type="submit" className="btn btn-primary">Create Account</button></div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )
